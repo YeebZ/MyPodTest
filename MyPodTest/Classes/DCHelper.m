96ad1dd5dc6c374f9d7941109e2c3dde4a36a5ca
zhangyibing <zhangyibing@zhangyibing.local>
2017-04-18 13:48:58 +0800

    0.1.0

---
+++ MyPodTest/Classes/DCHelper.m
@@ -0,0 +1,18 @@
+//
+//  DCHelper.m
+//  DachenKit
+//
+//  Created by 曾昭英 on 2016/12/23.
+//  Copyright © 2016年 Joey. All rights reserved.
+//
+
+#import "DCHelper.h"
+
+@implementation DCHelper
+
++ (void)showLog:(NSString *)text
+{
+    NSLog(@"dc 0.1.2 kit log :%@", text);
+}
+
+@end
